51d55d7a94d4b67e9fe10dfcd91dc733a1166653
MahmoudMatar <mahmoudmatar49@gmail.com>
2023-10-26 19:37:37 -0700

    Save files

--- Behavior Design Patterns/Command Design Pattern/src/Main.java
+++
@@ -1,9 +0,0 @@
-import concrete.lightcommand.TurnOffCommandLight;
-import concrete.lightcommand.TurnOnCommandLight;
-import recivers.Light;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
