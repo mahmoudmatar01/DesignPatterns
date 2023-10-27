d2c0164c24b2c5d0622c1b826d418c5b73829941
MahmoudMatar <mahmoudmatar49@gmail.com>
2023-10-26 20:05:18 -0700

    add chain of responsibility example

--- Behavior Design Patterns/Chain of Responsibility/src/Handler.java
+++ Behavior Design Patterns/Chain of Responsibility/src/Handler.java
@@ -1,10 +1,7 @@
 public abstract class Handler {
-
     public Handler successor=null;
-
     public void setSuccessor(Handler successor) {
         this.successor = successor;
     }
-
     public abstract void handleRequest(Request request);
 }
